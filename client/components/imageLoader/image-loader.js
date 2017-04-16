ac4f2452eaa0e42077afa7f2ff178397d6961b02
Eric Ewe <eric.ewe@zalora.com>
2017-04-16 18:54:03 +0800

    Create image loader and contents

--- client/components/imageLoader/image-loader.js
+++ client/components/imageLoader/image-loader.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import data from '../../database/data.json';
 
 const ImageLoader = () => (
-  <div>Images Here!</div>
+  <div id="image-loader-body">
+    {
+      data.data.map(product =>
+        <div key={product.id} id="product">
+          <img src="http://placehold.it/150x150" alt={product.attributes.name} />
+          <div id="product-name">{product.attributes.name}</div>
+          <div id="product-category">{product.attributes.category}</div>
+          <div id="product-price">${product.attributes.price / 100}</div>
+        </div>,
+      )
+    }
+  </div>
 );
 
 export default ImageLoader;
